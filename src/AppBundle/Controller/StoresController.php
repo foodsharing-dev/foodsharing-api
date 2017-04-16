f1daf6f197a251a6a8f9a8295b2f1b47be3802a3
Matthias Larisch <mail@matthias-larisch.de>
2017-04-17 01:17:48 +0200

    Implement get store detail

--- src/AppBundle/Controller/StoresController.php
+++ src/AppBundle/Controller/StoresController.php
@@ -6,7 +6,9 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Store;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class StoresController extends FOSRestController
@@ -18,10 +20,25 @@ class StoresController extends FOSRestController
      * @View(statusCode=200, serializerGroups={"userId", "storeDetail", "conversationList"})
      * @Get("/api/v1/stores")
      */
-    public function listNextAction(UserInterface $user = null)
+    public function listAction(UserInterface $user = null)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Store');
         $stores = $repository->findAllForUserOrderedByName($user->getId());
         return ['stores' => $stores];
     }
+
+    /**
+     * Get detailed information on a specific store.
+     *
+     * @ApiDoc()
+     * @View(statusCode=200, serializerGroups={"profile", "userProfileStore", "storeDetail", "conversationList"})
+     * @Get("/api/v1/stores/{id}")
+     * @Security("is_granted('view', store)")
+     */
+    public function getAction(Store $store)
+    {
+        return ['stores' => $store];
+    }
+
+
 }
--- src/AppBundle/Entity/StoreTeam.php
+++ src/AppBundle/Entity/StoreTeam.php
@@ -38,6 +38,7 @@ class StoreTeam
     private $coordinator;
 
     /**
+     * @Groups({"storeDetail"})
      * @ORM\Column(type="integer", name="active")
      */
     private $status;
