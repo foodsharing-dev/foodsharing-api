9bcdaec51019408f6cc754c6ab29467aa414f3f1
Matthias Larisch <mail@matthias-larisch.de>
2017-04-21 16:35:03 +0200

    Fix UserFactory fields

--- src/AppBundle/tests/_support/factories/UserFactory.php
+++ src/AppBundle/tests/_support/factories/UserFactory.php
@@ -3,16 +3,11 @@ use League\FactoryMuffin\Faker\Facade as Faker;
 use AppBundle\Entity\User as User;
 use AppBundle\Foodsharing;
 
-
 $fm->define(User::class)->setDefinitions([
     'firstName'   => Faker::firstName(),
-    'firstName'   => Faker::lastName(),
-    'email' => Faker::email(),
-
-    // generate email
+    'lastName'   => Faker::lastName(),
     'email'  => Faker::email(),
     'clearPassword' => Faker::password(),
-    'body'   => Faker::text(),
     'verified' => true,
     'active' => true,
     'password' => function ($object, $saved) {
