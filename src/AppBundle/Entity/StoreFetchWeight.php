29610d20803b2d68325957b8b9de7bf1296cbd37
Matthias Larisch <mail@matthias-larisch.de>
2017-04-23 15:34:05 +0200

    Add StoreFetchWeight entity (fs_abholmenge)
    That table is just used as an easy mapper to know the average fetch
    weights also in SQL. We make use of it here as well as the stored value
    inside the store is actually just a reference to this.

---
+++ src/AppBundle/Entity/StoreFetchWeight.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="fs_abholmengen")
+ */
+class StoreFetchWeight
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=1, name="menge")
+     */
+    private $averageWeight;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set averageWeight
+     *
+     * @param string $averageWeight
+     *
+     * @return StoreFetchWeight
+     */
+    public function setAverageWeight($averageWeight)
+    {
+        $this->averageWeight = $averageWeight;
+
+        return $this;
+    }
+
+    /**
+     * Get averageWeight
+     *
+     * @return string
+     */
+    public function getAverageWeight()
+    {
+        return $this->averageWeight;
+    }
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return StoreFetchWeight
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
